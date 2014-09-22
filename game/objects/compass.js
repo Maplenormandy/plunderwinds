d682ed11f1119d18edb4a485875de45e09d1a362
Zygimantas Straznickas <sprinterlt@gmail.com>
2014-09-22 15:14:41 -0400

    Added direction to compass object.

--- game/objects/compass.js
+++ game/objects/compass.js
@@ -1,19 +1,25 @@
 'use strict';
 var Sprite = require('./sprite');
 
+/*
+  A compass object
+
+  has a 'direction' property (0 - 7, 0 - north, increases clockwise)
+*/
+
 function Compass(game, x, y) {
   // this.grid = grid;
   this.x = x;
   this.y = y;
   this.game = game;
   Sprite.call(this, game, x, y, 'compass');
-  // this.phSprite.anchor.setTo(0);
   this.phSprite.scale.setTo(0.4, 0.4);
 
   this.arrow = this.game.add.sprite(this.x, this.y, 'compass_arrow');
   this.arrow.anchor.setTo(0.5, 1);
 
-  // this.pointTo(4);
+  this.direction = 0;
+
   
 }
 
@@ -22,6 +28,8 @@ Compass.prototype = new Sprite();
 Compass.prototype.constructor = Compass;
 
 Compass.prototype.pointTo = function(dir) {
+  this.direction = dir;
+
   var target = (dir % 8)/8*360;
   this.game.add.tween(this.arrow)
   .to({angle: target}, 500, Phaser.Easing.Back.Out)
