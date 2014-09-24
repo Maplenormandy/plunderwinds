bea9d5f15b5035694ed40d6bccde43d3f1bcadb2
Bennett Cyphers <bcyphers@mit.edu>
2014-09-24 14:36:16 -0400

    Moving against the wind does trigger game over

--- game/states/play.js
+++ game/states/play.js
@@ -123,15 +123,16 @@ Play.prototype = {
     outcome.effectFunc(this.ship, this.encounterManager);
     this.state = this.STATES.STANDBY;
     this.sidePanel.update();
-    if (this.ship.stamina <= 0) {
+    if (this.ship.stamina <= 0)
       this.game.state.start('gameover', true, false, this);
-    }
   },
 
   failToMove: function(me) {
     //console.log("Fighting the wind!");
     me.state = me.STATES.STANDBY;
     this.sidePanel.update();
+    if (this.ship.stamina <= 0) 
+      this.game.state.start('gameover', true, false, this);
   },
 
   clickListener: function() {
