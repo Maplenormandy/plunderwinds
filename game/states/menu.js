d1e3c07cb42b5e4909d072ea9822a50abbb5e478
Peter Schmidt-Nielsen <snp@mit.edu>
2014-09-24 14:34:01 -0400

    Added simple description string to splash page.

--- game/states/menu.js
+++ game/states/menu.js
@@ -20,7 +20,7 @@ Menu.prototype = {
     this.titleText = this.game.add.text(this.game.world.centerX, 300, 'Plunder Winds', style);
     this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'click to play fun pirate game yes', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, "Your goal is to maximize gold before running out of stamina.\nClick on a tile you've never been to before adjacent to your current location to move there.\nMovement opposing the wind (green arrow in lower right) is prohibited,\nand movement not with the wind costs stamina.", { font: '16px Arial', fill: '#ffffff', align: 'center'});
     this.instructionsText.anchor.setTo(0.5, 0.5);
 
     this.sprite.angle = -20;
