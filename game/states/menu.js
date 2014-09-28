1b0a393daa786f74b3ef1fda486bc4447f118617
Bennett Cyphers <bcyphers@mit.edu>
2014-09-28 19:48:56 -0400

    Instructions update
    Adding more detailed instructions, as well as a way to click through from page
    to page.

--- game/states/menu.js
+++ game/states/menu.js
@@ -8,6 +8,29 @@
 function Menu() {}
 
 Menu.prototype = {
+  instructions: [
+"The winds of change are blowing, and the high seas are ripe with dubloons! In\n\
+Plunder Winds, you must lead your crew through troubled waters, hunting for\n\
+booty and avoiding hazards like hostile pirates and the Royal Navy. Reach safe\n\
+harbor, and your journey ends in triumph: you and your crew split your reward.\n\
+Run out of stamina, and your story ends adrift in the ocean, as you pray for the\n\
+mercy of a passing merchant or surrender to death's sweet relief in the bosom of\n\
+the sea.\n\nClick to continue.",
+
+"Move to a tile directly adjacent to you (up/down/left/right) by clicking on it,\n\
+or using the arrow keys to navigate. You may travel with the wind for free, or\n\
+use stamina to fight through cross winds. But beware: sailing directly against\n\
+the wind is impossible, and will cost you stamina without getting you anywhere.\n\
+The royal navy is in hot pursuit, so you may not cross your own path.\n\n\
+Click to continue.",
+
+"Each section of ocean is marked with a color indicating its risk level. For\n\
+example, green waters are friendly, and have a greater chance of reward; waters\n\
+marked red are more treacherous. Your goal is a friendly port somewhere on the\n\
+edge of the map. When you reach it, the game ends, and your score is calculated\n\
+based on how much loot you have collected.\n\
+Good luck.\n\nClick to continue."
+  ],
   preload: function() {
     
   },
@@ -17,18 +40,32 @@ Menu.prototype = {
     this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'pirate');
     this.sprite.anchor.setTo(0.5, 0.5);
 
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, 'Plunder Winds', style);
+    this.titleText = this.game.add.text(this.game.world.centerX, 300, 
+                                        'Plunder Winds', style);
     this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, "Your goal is to maximize gold before running out of stamina.\nClick on a tile you've never been to before adjacent to your current location to move there.\nMovement opposing the wind (green arrow in lower right) is prohibited,\nand movement not with the wind costs stamina.", { font: '16px IM Fell English SC', fill: '#ffffff', align: 'center'});
+    this.ins_idx = 0;
+    this.instructionsText = this.game.add.text(this.game.world.centerX, 450,
+                                               this.instructions[this.ins_idx], 
+                                               { font: '18px IM Fell English SC',
+                                                 fill: '#ffffff', 
+                                                 align: 'center' });
     this.instructionsText.anchor.setTo(0.5, 0.5);
 
     this.sprite.angle = -20;
-    this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+    this.game.add.tween(this.sprite).to({angle: 20}, 1000,
+                                        Phaser.Easing.Linear.NONE, 
+                                        true, 0, 1000, true);
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
-      this.game.state.start('play');
+      this.ins_idx += 1;
+
+      if (this.ins_idx < this.instructions.length) {
+        this.instructionsText.text = this.instructions[this.ins_idx];
+      } else {
+        this.game.state.start('play');
+      }
     }
   }
 };
