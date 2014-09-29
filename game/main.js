4c25ddca357f17a4425aef7c5a4981aa70839c98
Peter Schmidt-Nielsen <snp@mit.edu>
2014-09-29 02:47:19 -0400

    Added changes making high scores visible on main page.

--- game/main.js
+++ game/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var jQuery = require('./lib/jquery-1.11.1.min.js');
+
 //global variables
 window.onload = function () {
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'plunderwinds');
@@ -12,4 +14,18 @@ window.onload = function () {
   game.state.add('preload', require('./states/preload'));
   
   game.state.start('boot');
+
+  // Populate the highscore table.
+  jQuery.ajax({
+    url: 'http://snp.scripts.mit.edu/get_scores_plunderwinds.py',
+    success: function(data) {
+      var string = "";
+      for (var i = 0; i < 3; i++) {
+        string += data[i][0] + ": " + data[i][1] + " gold";
+        if (i != 2)
+          string += " - ";
+      }
+      jQuery("#highscoreboard").text(string).html();
+    }
+  });
 };
